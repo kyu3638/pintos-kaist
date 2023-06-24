35b5b1b561a82f6b10c15e8025d755bb1e184af3
huir0 <keronruss@gmail.com>
2023-06-24 17:40:49 +0000

    before mmap

--- vm/vm.c
+++ vm/vm.c
@@ -184,11 +184,12 @@ bool vm_try_handle_fault(struct intr_frame *f UNUSED, void *addr UNUSED, bool us
 	struct page *page = NULL;
 	/* TODO: Validate the fault */
 	/* TODO: Your code goes here */
-	if (is_kernel_vaddr(addr))
+	if (is_kernel_vaddr(addr) || !not_present)
 	{
 		return false;
 	}
 	page = spt_find_page(spt, addr);
+
 	if(page == NULL){
 		if(addr < USER_STACK && addr >= USER_STACK-(1<<20) && f->rsp - (1<<3) <= addr && addr <= thread_current()->stack_bottom){
 			vm_stack_growth(addr);
