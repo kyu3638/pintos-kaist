02ed493b429c7dc6372f199ea091bcadabaec09d
Ubuntu <ubuntu@ip-172-31-6-216.ap-northeast-2.compute.internal>
2023-06-25 00:03:40 +0000

    1

--- vm/vm.c
+++ vm/vm.c
@@ -228,7 +228,6 @@ void vm_dealloc_page(struct page *page)
 bool vm_claim_page(void *va UNUSED)
 {
 	struct page *page = spt_find_page(&thread_current()->spt, va);
-	/* TODO: Fill this function */
 	if (page == NULL)
 	{
 		return false;
